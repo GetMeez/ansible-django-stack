637ba12d6816006ea734590ae8c97ebaf423503a
Ian Campbell <ipcampbell@gmail.com>
2019-01-20 18:40:08 -0500

    Update bin files to use ssh-agent forwarding.

--- bin/staging/deploy-meez-staging.sh
+++ bin/staging/deploy-meez-staging.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i staging webservers.yml -K --tags="deploy" --ask-vault-pass
+ansible-playbook --ssh-extra-args=-A -i staging webservers.yml -K --tags="deploy" --ask-vault-pass
--- bin/staging/setup-meez-staging.sh
+++ bin/staging/setup-meez-staging.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i staging webservers.yml -K --ask-vault-pass
+ansible-playbook --ssh-extra-args=-A -i staging webservers.yml -K --ask-vault-pass
